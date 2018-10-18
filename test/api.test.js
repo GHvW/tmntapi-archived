f00e75a965e071dccff6c96a3eb17e921869442e
ghvw <ghvanw283@gmail.com>
2018-10-17 22:29:13 -0500

    get basic test working

---
+++ test/api.test.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const server = require("../src/server");
+const assert = require("assert");
+
+// const assert = chai.assert;
+
+describe("/GET API", () => {
+  it("checks equality for now", () => {
+    assert.equal(true, true);
+  });
+});
