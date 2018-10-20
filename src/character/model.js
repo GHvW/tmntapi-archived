b734a1e9eec9dc5bd0c1534241243be99dfce28b
ghvw <ghvanw283@gmail.com>
2018-10-19 22:49:49 -0500

    update model to remove description change weapon to gear

--- src/character/model.js
+++ src/character/model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const characterSchema = new mongoose.Schema({
   name: String,
   biology: String,
-  weapon: String,
-  description: String
+  gear: [String],
 });
 
 //"character" needs to match the collection name
---
+++ test/character.test.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = reqiure("../server");
+
+const assert = chai.assert;
+
+describe("character endpoint tests", () => {
+  describe("GET all characters", () => {
+    it("should get all characters")
+  })
+})
