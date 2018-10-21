deb8e986996d06f589aec5f403a0dc01a8dcda7d
ghvw <ghvanw283@gmail.com>
2018-10-20 22:30:06 -0500

    add / route with base endpoints

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log(`Connection to Mongo DB successful at ${db}`))
   .catch(err => console.log(`Error connecting to Mongo DB: ${err}`));
 
+const SITE_URL = "http://localhost:1337/api";
+
 router.use("/character", character);
 
 router.use("/pizza", pizza);
@@ -24,6 +26,16 @@ router.use("/tech", tech);
 router.use("/vehicle", vehicle);
 
 router.use("/series", series);
+
+router.use("/", (req, res) => {
+  res.send({
+    characters: `${SITE_URL}/character/`,
+    pizzas: `${SITE_URL}/pizza/`,
+    tech: `${SITE_URL}/tech/`,
+    vehicles: `${SITE_URL}/vehicle/`,
+    series: `${SITE_URL}/series/`
+  });
+});
 
 app.use("/api", router);
 
